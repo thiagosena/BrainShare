cf19d8225c64a852d99473ab6f6f84080501cb0c
Thiago César <lord.sena@gmail.com>
2013-05-27 09:16:51 -0300

    Adição da págiona question.xhtml

--- JavaSource/br/brainshare/model/QuestionBean.java
+++ JavaSource/br/brainshare/model/QuestionBean.java
@@ -1,13 +1,15 @@
 package br.brainshare.model;
 
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
@@ -29,16 +31,28 @@ public class QuestionBean {
 	@Column(name = "data_cadastro")
 	private Date dateRegister;
 	
-	@ManyToOne
+	@ManyToMany
 	@JoinColumn(name = "id_tag", nullable = false)
-	private TagBean tag;
+	private List<TagBean> tags;
+	
+	@OneToOne
+	@JoinColumn(name = "id_user", nullable = true)
+	private UserBean user;
 
-	public TagBean getTag() {
-		return tag;
+	public UserBean getUser() {
+		return user;
 	}
 
-	public void setTag(TagBean tag) {
-		this.tag = tag;
+	public void setUser(UserBean user) {
+		this.user = user;
+	}
+
+	public List<TagBean> getTags() {
+		return tags;
+	}
+
+	public void setTags(List<TagBean> tags) {
+		this.tags = tags;
 	}
 
 	public Integer getId() {
